3e224396473f0acf4c2172e00a65efb5c26a07d0
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-05-21 16:15:14 +0300

    add dicts.py, passed test_dicts.py 100%

---
+++ tests/test_dicts.py
@@ -0,0 +1,10 @@
+from utils.dicts import get_val
+
+
+def test_get_val():
+    assert get_val({"vcs": "mercurial"}, "vcs") == 'mercurial'
+    assert get_val({}, "test", "git") == "git"
+    assert get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert get_val({}, "vcs", "bazaar") == "bazaar"
+
+
